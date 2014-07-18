547661105a925aef25cd5e10cf4d456245f9ffc8
DanOvando <dovando@bren.ucsb.edu>
2014-07-18 14:16:34 -0700

    Updating ability to set functions in master, and use of wrapper

--- GFR_Master.R
+++ GFR_Master.R
@@ -5,6 +5,8 @@
 ######################################
 rm(list=ls())
 library(car)
+library(plyr)
+library(lattice)
 # Basic Controls -------------------------------------------------------------
 
 BatchFolder<- 'Results/Scratch Folder/'
@@ -20,8 +22,8 @@ dir.create(BatchFolder)
 dir.create(FigureFolder)
 
 dir.create(ResultFolder)
-  
-  
+
+
 #Output storage
 
 #Sections to run/or try and call
@@ -33,7 +35,18 @@ dir.create(ResultFolder)
 
 # Data Processing ---------------------------------------------------------
 
-#Options for processing RAM, SOFIA, FAO
+MinimumCatchYears<- 5 #Minimum length of catch history
+
+FisheriesToOmit<- 'None' #List of fisheries to manually exclude from analysis 
+
+SpeciesCategoriesToOmit<- c('Corals','Frogs and other amphibians','Eared seals, hair seals, walruses',
+                            'Turtles','Pearls, mother-of-pearl, shells','Crocodiles and alligators','Miscellaneous aquatic plants'
+                            ,'Freshwater crustaceans','Sperm-whales, pilot-whales','Green seaweeds','Red seaweeds',
+                            'Brown seaweeds','Sea-squirts and other tunicates','Blue-whales, fin-whales',
+                            'Miscellaneous aquatic mammals','Sponges','Krill, planktonic crustaceans')
+
+MissingCatchTolerance<- 0.99 #Maximumum percentage of catch years that can be missing
+
 
 # Regressions -------------------------------------------------------------
 
@@ -45,8 +58,6 @@ DependentName<- DependentVariable
 
 DependentName<- if (IsLog==T){paste('Log',DependentVariable,sep='')}
 
-
-  
 CatchLags<- 4 #Number of years of lagged catch to create for regression
 
 LifeHistoryVars<- c('MaxLength','AgeMat','VonBertK') #Life history variables to include for potential regression
--- GFR_Wrapper.R
+++ GFR_Wrapper.R
@@ -11,11 +11,25 @@ sapply(list.files(pattern="[.]R$", path="Functions", full.names=TRUE), source);
 
 # Read in and process data ------------------------------------------------------------
 
-source('Database_Build.r') #Build Tyler's database
+# source('Database_Build.r') #Build Tyler's database
+
+fulldata<- read.csv(paste(ResultFolder,'Raw Compiled Database.csv',sep=''))
 
 FullData<- fulldata
 
 rm(fulldata)
+
+CleanedData<- MaidService(FullData)
+
+DroppedStocks<- CleanedData$DroppedStocks
+
+FullData<- CleanedData$CleanedData
+
+write.csv(file=paste(ResultFolder,'Raw Compiled Database.csv',sep=''),FullData)
+
+write.csv(file=paste(ResultFolder,'Omitted Stocks.csv',sep=''),DroppedStocks)
+
+rm(CleanedData)
 
 FullData$ReferenceBiomass[FullData$ReferenceBiomass==0]<- NA
 
@@ -23,24 +37,33 @@ FullData$Biomass<- as.numeric(FullData$Biomass)
 
 FullData$BvBmsy<- FullData$Biomass/FullData$ReferenceBiomass
 
-head(FullData)
 
 # Create synthetic stocks -------------------------------------------------
 
+
+
+RamData<- FullData[FullData$Dbase=='RAM',]
+
 if (Groups=='All')
 {
-Groups<- unique(FullData$SpeciesCatName,na.rm=T)
-
-Groups<- Groups[is.na(Groups)==F]
+  Groups<- unique(RamData$SpeciesCatName,na.rm=T)
+  
+  Groups<- Groups[is.na(Groups)==F]
 }
 
-SyntheticStocks<- StitchFish(FullData,IdVar,Groups,GroupSamples,Iterations)
+SyntheticStocks<- StitchFish(RamData,IdVar,Groups,GroupSamples,Iterations)
 
 # Prepare data for regression ---------------------------------------------
 
-RamData<- FullData[FullData$Dbase=='RAM', ]
+library(proftools)
+
+ Rprof()
 
 RegressionData<- FormatForRegression(RamData,DependentVariable,CatchLags,LifeHistoryVars,IsLog,IdVar)
+ Rprof(NULL)
+ RProfData<- readProfileData('Rprof.out')
+ flatProfile(RProfData,byTotal=TRUE)
+
 
 SyntheticRegressionData<- FormatForRegression(SyntheticStocks,DependentVariable,CatchLags,LifeHistoryVars,IsLog,IdVar)
 
@@ -51,6 +74,8 @@ RealModels<- RunRegressions(RegressionData,Regressions,'Real Stocks')
 NeiModels<- RunRegressions(SyntheticRegressionData,Regressions,'Synthetic Stocks')
 
 # Apply regressions -------------------------------------------------------
+
+unassessed<- predict(RealModels$M6,RegressionData)
 
 # Create stitched database ------------------------------------------------
 
