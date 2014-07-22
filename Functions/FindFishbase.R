cdc785b3121a43db4431fdfc962e6bdb7b4cac59
tclavelle <tclavelle@bren.ucsb.edu>
2014-07-21 17:10:38 -0700

    removed 0's from reference values and updated FishBase function

--- Functions/FindFishbase.R
+++ Functions/FindFishbase.R
@@ -6,11 +6,38 @@
 FindFishbase<- function(Data,LifeVars)
 {
   
-  #Pass FullData
+  Data<-FullData
   
  load('Data/mpack.Rdata')
   
  Fishbase<- mpack$lh
 
  rm(mpack)
+ 
+ spnames<-unique(Data$SciName)
+
+for (i in 1:length(spnames)){
+  
+  lh_match=match(Data$SciName[i],Fishbase$sname) #find if species name name matches FB data, locate row
+  WhereVb<-spnames[i]==Data$SciName & is.na(Data$VonBertK)
+  WhereAge<-spnames[i]==Data$SciName & is.na(Data$AgeMat)
+  WhereMaxL<-spnames[i]==Data$SciName & is.na(Data$MaxLength)
+  WhereTemp<-spnames[i]==Data$SciName & is.na(Data$Temp)
+  
+  if (is.na(lh_match)==F)
+  {
+    lh_data=Fishbase[lh_match,] #pull out matching data
+    spcat=lh_data$spcat #identify the species category
+    
+    #Add in life history
+    Data$MaxLength[WhereMaxL]=lh_data$maxl
+    Data$VonBertK[WhereVb]=lh_data$vbk
+    Data$AgeMat[WhereAge]=lh_data$agem
+    Data$Temp[WhereTemp]=lh_data$temp
+ 
+  # function taking a VERY long time, not confirmed that it's working
+  }
+}
+
+
 }
