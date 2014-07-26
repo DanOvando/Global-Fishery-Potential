523882b218cf47c4a8ef210d6c698484ef979c1f
DanOvando <dovando@bren.ucsb.edu>
2014-07-26 09:33:16 +0700

    sped up code and removed unneeded crap

--- Functions/FindFishbase.R
+++ Functions/FindFishbase.R
@@ -6,38 +6,57 @@
 FindFishbase<- function(Data)
 {
     
- 
+#   Data<- FullData
   load('Data/mpack.Rdata')
   
  Fishbase<- mpack$lh
 
+ Fishbase[is.na(Fishbase)]= NA
  rm(mpack)
  
- spnames<-unique(Data$SciName)
+ spnames<-as.character(levels((Data$SciName)))[Data$SciName]
 
+ spnames<- unique(spnames)
+ 
+ Data$VonBertKSource<- as.character(levels(Data$VonBertKSource))[Data$VonBertKSource]
+
+ Data$AgeMatSource<- as.character(levels(Data$AgeMatSource))[Data$AgeMatSource]
+ 
+ Data$MaxLengthSource<- as.character(levels(Data$MaxLengthSource))[Data$MaxLengthSource]
+ 
 for (i in 1:length(spnames)){
   
-  lh_match=match(spnames[i],Fishbase$sname) #find if species name name matches FB data, locate row
-  WhereVb<-spnames[i]==Data$SciName & is.na(Data$VonBertK)
-  WhereAge<-spnames[i]==Data$SciName & is.na(Data$AgeMat)
-  WhereMaxL<-spnames[i]==Data$SciName & is.na(Data$MaxLength)
-  WhereTemp<-spnames[i]==Data$SciName & is.na(Data$Temp)
+  show(paste(100*(round(i/length(spnames),2)),'% Done',sep=''))
+  
+  lh_match=grepl(spnames[i],Fishbase$sname) #find if species name name matches FB data, locate row
+ 
+  if (sum(lh_match)>0)
+  {
+  
+    WhereSpecies<- spnames[i]==Data$SciName
+#   WhereVb<- spnames[i]==Data$SciName & is.na(Data$VonBertK)
+  WhereVb<- WhereSpecies & is.na(Data$VonBertK)
+  
+  WhereAge<-  WhereSpecies & is.na(Data$AgeMat)
+  WhereMaxL<- WhereSpecies & is.na(Data$MaxLength)
+  WhereTemp<- WhereSpecies & is.na(Data$Temp)
   
     lh_data=Fishbase[lh_match,] #pull out matching data
-    spcat=lh_data$spcat #identify the species category
+#     spcat=lh_data$spcat #identify the species category
     
     #Add in life history
-    Data$MaxLength[WhereMaxL]<-lh_data$maxl
+    Data$MaxLength[WhereMaxL]<- lh_data$maxl[1]
     Data$MaxLengthSource[WhereMaxL]<-"FishBase"
-    Data$VonBertK[WhereVb]<-lh_data$vbk
+    
+  Data$VonBertK[WhereVb]<-lh_data$vbk[1]
     Data$VonBertKSource[WhereVb]<-"FishBase"
-    Data$AgeMat[WhereAge]<-lh_data$agem
+    Data$AgeMat[WhereAge]<-lh_data$agem[1]
     Data$AgeMatSource[WhereAge]<-"FishBase"
-    Data$Temp[WhereTemp]<-lh_data$temp
+    Data$Temp[WhereTemp]<-lh_data$temp[1]
     Data$TempSource[WhereTemp]<-"FishBase" 
- 
-print(i)
-}
+ } #close if loop
+
+} #close species loop
 
 return(Data)
 
