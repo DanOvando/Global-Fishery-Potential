6aaaacf8cac05572bd106df3e5139348f58a2821
DanOvando <dovando@bren.ucsb.edu>
2014-08-02 22:05:11 +0800

    housekeeping

---
+++ Functions/AssignEconomicData.R
@@ -0,0 +1,44 @@
+
+AssignEconomicData<- function(Data)
+{
+  
+#   Data<- FullData
+  
+  PriceData<- read.csv('Data/Species Category Prices.csv')
+  
+  BvBmsyOpenAccess<- read.csv('Data/Species Category Open Access Depletion.csv')
+  
+  SpeciesCats<- unique(Data$SpeciesCatName)
+    
+  for (s in 1:length(SpeciesCats))
+  {
+    
+    WhereSpecies<- Data$SpeciesCatName==SpeciesCats[s]
+        
+    WherePrices<- as.character(PriceData$SpeciesCatName)==as.character(SpeciesCats[s])
+    
+    if (sum(WherePrices)>0)
+    {
+      Price<- (PriceData$Price[WherePrices])
+      
+    }
+    else{Price<- mean(PriceData$Price,na.rm=T)}
+    
+    WhereBOA<- (BvBmsyOpenAccess$SpeciesCatName)==as.character(SpeciesCats[s])
+    
+    if (sum(WhereBOA)>0)
+    {
+      BOA<- (BvBmsyOpenAccess$BvBmsyOpenAccess[WhereBOA])
+    }
+    else
+    {
+      BOA<- mean(BvBmsyOpenAccess$BvBmsyOpenAccess,na.rm=T)
+    }
+    
+    Data$Price[WhereSpecies]<- Price
+    
+    Data$BvBmsyOpenAccess[WhereSpecies]<- BOA
+  } #Close species category loop
+  
+  return(Data)
+} #close function
