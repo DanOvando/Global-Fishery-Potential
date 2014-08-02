0a4d4c8d4e715004e45722b112676272115b4264
DanOvando <dovando@bren.ucsb.edu>
2014-08-02 22:04:45 +0800

    added mods

--- Functions/GFRM_funR.R
+++ Functions/GFRM_funR.R
@@ -8,7 +8,8 @@ GFRM_funR= function(f,b,p,MSY,c,r,beta,V,bvec,delta)
 {  
   profit= p*MSY*f*b - c*(f*r/2)^beta
 
-bnext= b + r*b*(1-b/2) - r/2*b*f
+bnext= max(min(bvec),b + r*b*(1-b/2) - r/2*b*f)
+bnext=min(max(bvec),bnext)
 out= approx(bvec,t(V),bnext) #spline(bvec,V,xout=bnext)
 Vnext= out$y
 
