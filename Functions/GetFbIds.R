cf8d4ff90398934199054dd769fb528278d4f54c
tclavelle <tclavelle@bren.ucsb.edu>
2014-08-11 16:50:20 -0700

    updated live fishbase scraping code with required sub functions. Master updated to include required packages

---
+++ Functions/GetFbIds.R
@@ -0,0 +1,83 @@
+#################################################################
+# Script for Obtaining Fishbase codes for species in dataset
+# Original code by Jorge Cornejo
+# Adapted by Tyler Clavelle
+#
+# Purpose: Function takes finds Fishbase codes for all available species in dataset. Writes .csv for use by FindFishBase function
+#
+#################################################################
+
+GetFbIds<-function(FullData){
+
+install <- 1 ## Change this to 0 after the first intalation!!
+
+if (install == 0)
+{
+  install.packages("rfishbase")
+  install.packages("rfishbase")
+  install.packages("XML")
+  install.packages("stringr")
+  install.packages('RCurl')
+}
+
+# require(rfishbase)
+# require(stringr)
+
+## This source command load the files with the Fish Base functions! 
+# source calls not neccessary once functions added to Function folder on Git
+
+# source(fb.ids.R)
+# source(fb_growth.R)
+# source(fb_maturity.R)
+# source(fb_agesize.R)
+
+## Now, using the 'rfishbase' package, get the list of species to obtain the data from..
+
+data(fishbase)
+## All the species listed in FB that are part of Engraulidae family
+# sp <- which_fish("Engraulidae", using="Family", fish.data) 
+# spList <- fish_names(fish.data[sp]) ## This is a vector with all the scientific names!
+
+spList<-unique(FullData$SciName) # get vector with all scientific names in dataset
+# clean species names so that HTML parsing doesn't fail
+spList<-gsub(".","",spList,fixed= T) # remove periods
+spList<-gsub(",","",spList) # remove commas
+spList<-gsub("\\(.*\\)","",spList) # delete anything within parentheses
+spList<-gsub("  "," ",spList) # convert any double spaces to single spaces
+spList<- gsub("^\\s+|\\s+$","",spList) # trim leading and trailing space
+
+temp <- str_split(string=spList, pattern=" ")
+n <- length(spList)
+ScNames <- data.frame(Genus = rep(NA, n), Species= rep(NA, n))
+ScNames$idFB <- NA
+ScNames$StockCode <- NA 
+
+## This loop is to obtain the number used by the FB to identify the fish. This is the number
+## that is used to call all the links to get the data!
+pb <- txtProgressBar(min = 0, max = 1356, style = 3)
+
+## I'm just doing this example for the first 5 species! 
+for (i in 1:length(spList))
+{
+  ScNames$Genus[i] <- Genus <-  temp[[i]][1]
+  ScNames$Species[i] <- Species <- temp[[i]][2]
+  ScNames$Species[i]<-tolower(Species) # make sure all species names are lower case
+  
+  if (is.na(temp[[i]][2])){
+    ScNames$Species[i] <-"spp"
+    temp[[i]][2]<-"spp"}
+  
+  if (temp[[i]][1]==""){
+    ScNames$Genus[i] <-"Blank"
+    temp[[i]][1]<-"Blank"}
+  
+  temp2 <- fb_ids(ScNames$Genus[i], ScNames$Species[i])
+  ScNames$idFB[i] <- temp2$idFB
+  ScNames$StockCode[i] <- temp2$StockCode
+  setTxtProgressBar(pb, i)
+}
+
+write.csv(file=paste(ResultFolder,"FB_IDS.csv",sep=""),ScNames) # write csv with FB codes so this script only needs to be run once
+
+return ()
+}
