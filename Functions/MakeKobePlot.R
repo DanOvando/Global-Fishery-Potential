1df5a88ca8a0c2ed89d794b692dfaec795fde69c
DanOvando <dovando@bren.ucsb.edu>
2014-08-27 09:59:04 -0700

    Improved figure

--- Functions/MakeKobePlot.R
+++ Functions/MakeKobePlot.R
@@ -25,7 +25,7 @@ DensityData<- (kde2d(BaselineData$BvBmsy,BaselineData$FvFmsy,n=500))
 
 FTrend<- ddply(Data,c('Year','Dbase'),summarize,FTrend=mean(FvFmsy,na.rm=T))
 
-KobeColors<- colorRampPalette(c("aliceblue", "white",'gold1'))
+KobeColors<- colorRampPalette(c("lightskyblue", "white",'gold1'))
 
 DbaseColors<- colorRampPalette(c('Red','Black','Green'))
 
@@ -33,13 +33,14 @@ DbaseColors<- colorRampPalette(c('Red','Black','Green'))
 
 pdf(file=paste(FigureFolder,FigureName,'.pdf',sep=''))
 # par(mfrow=c(1,2))
-layout(t(matrix(c(1,2))), widths = c(3.25,1), heights = c(1,1), respect = FALSE)
+layout(t(matrix(c(1,2))), widths = c(4,1), heights = c(1,1), respect = FALSE)
+par(mai=c(1,1,1,0))
 image(DensityData$x,DensityData$y,DensityData$z,col=KobeColors(50),xlab='B/Bmsy',ylab='F/Fmsy')
 points(BaselineData$BvBmsy,BaselineData$FvFmsy,pch=16,col=(as.factor(BaselineData$Dbase)))
 abline(h=1,v=1,lty=2)
 plot.new()
 par(mai=c(0,0,0,0))
-legend('center',legend=unique(BaselineData$Dbase),pch=16,col=(as.factor(unique(BaselineData$Dbase))),cex=0.65,bty='n')
+legend('center',legend=unique(BaselineData$Dbase),pch=16,col=(as.factor(unique(BaselineData$Dbase))),cex=0.7,bty='n')
 dev.off()
 
 
