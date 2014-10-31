1129f45eb6b14885bd34f4797eb1414ac6547cd7
DanOvando <dovando@bren.ucsb.edu>
2014-10-31 12:34:51 -0700

    HK

--- Diagnostics/JackknifePredictions.R
+++ Diagnostics/JackknifePredictions.R
@@ -31,139 +31,139 @@ for (r in 1:length(RamIds))
   
   if (sum(is.na(Omit$Catch))==0)
   {
-  
-  FirstCatch<- which(is.na(Omit$Catch)==F)[1]
-  
-  Omit<- Omit[(FirstCatch+4):dim(Omit)[1],]
-  
-  Omit$CatchToRollingMax[is.na(Omit$CatchToRollingMax)]<- 0
-  
-  Omit$Catch<- na.approx(Omit$Catch)
-  
-  Jacked<- RamData[RamData$IdOrig!=RamIds[r],]
-  
-  TempJack<- as.data.frame(matrix(NA,nrow=dim(Omit)[1],ncol=12))
-  
-  colnames(TempJack)<- c('Assessid','Year','RamB','RamF','RamMSY','PrmB','CmsyB','CmsyF','CmsyMSY','CmsyBnoP','CmsyFnoP','CmsyMSYnoP')
-  
-  TempJack[,c('Assessid','Year','RamB','RamF','RamMSY')]<- Omit[,c('IdOrig','Year','BvBmsy','FvFmsy','MSY')]
-  
-  JackModel<- RunRegressions(Jacked,Regressions,'Real Stocks')
-  
-  RealModelFactorLevels<- NULL
-  
-  Models<- names(Regressions)
-  
-  
-  TempOmitted<- NULL
-  
-  for (m in 1:length(names(Regressions)))
-  {
-    Model<- names(Regressions)[m]
-    eval(parse(text=paste('RealModelFactorLevels$',Model,'<- RealModels$',Model,'$xlevels$SpeciesCatName',sep='')))
-  }
-  
-  Jacked<- InsertFisheryPredictions(Jacked,JackModel) #Add fishery predictions back into main dataframe
-  
-  RealModelSdevs<- CreateSdevBins(JackModel,Jacked,TransbiasBin)
-  
-  
-  AllPossible<- unique(data.frame(I(Jacked$SpeciesCatName),I(Jacked$SpeciesCat)))
-  
-  colnames(AllPossible)<- c('SpeciesCatNames','SpeciesCat')
-  
-  RamPossibleCats<- unique(RamData$SpeciesCatName)
-  
-  Models<- Models[Models!='M7']
-  
-  for (m in 1:length(Models)) #Apply models to species level fisheries
-  {
     
-    TempModelName<- Models[m]
+    FirstCatch<- which(is.na(Omit$Catch)==F)[1]
     
-    eval(parse(text=paste('TempLevel<- RealModelFactorLevels$',TempModelName,sep='')))
+    Omit<- Omit[(FirstCatch+4):dim(Omit)[1],]
     
-    eval(parse(text=paste('TempModel<- RealModels$',TempModelName,sep='')))
+    Omit$CatchToRollingMax[is.na(Omit$CatchToRollingMax)]<- 0
     
-    ProxyCats<- AssignNearestSpeciesCategory(Omit,TempLevel,AllPossible)
+    Omit$Catch<- na.approx(Omit$Catch)
     
-    Predictions<- predict(TempModel,ProxyCats$Data)
+    Jacked<- RamData[RamData$IdOrig!=RamIds[r],]
     
-    eval(parse(text=paste('Omit$',TempModelName,'Prediction<- Predictions',sep='')))  
-  }
-  
-  BiomassData<- Omit #Only store fisheries that have some form of biomass estimates
-  
-  BiomassData$BvBmsy<- NA
-  
-  BiomassData$BestModel<- NA
-  
-  BiomassData$LogBvBmsy<- NA
-  
-  BiomassData$FvFmsy<- NA
-  
-  BiomassData$MSY<- NA
-  
-  BiomassColumns<- (grepl('BvBmsy',colnames(Omit)) | grepl('Prediction',colnames(Omit))) & grepl('LogBvBmsy',colnames(Omit))==F
-  
-  AvailableBio<- (BiomassData[,BiomassColumns])
-  
-  AvailableBioMarker<- matrix(rep((1:dim(AvailableBio)[2]),dim(AvailableBio)[1]), dim(AvailableBio)[1],dim(AvailableBio)[2],byrow=TRUE)
-  
-  AvailableBioMarker<- AvailableBioMarker*(is.na(AvailableBio)==F)
-  
-  AvailableBioMarker[AvailableBioMarker==0]<- NA
-  
-  BestModel<- apply(AvailableBioMarker,1,min,na.rm=T)
-  
-  BestBio<- NULL
-  for (b in 1:dim(AvailableBio)[1])
-  {
-    BestBio[b]<- AvailableBio[b,BestModel[b]]
-  }
-  
-  BestBio[BestModel==1]<- log(BestBio[BestModel==1])
-  
-  BestModelnames<- c('RAM',ModelNames)
-  
-  BestModelNames<- BestModelnames[sort(unique(BestModel))]
-  
-  BestModel<- as.factor((BestModel))
-  
-  levels(BestModel)<- BestModelNames
-  
-  BiomassData$BestModel<- BestModel
-  
-  BiomassData$BvBmsy<- BestBio
-  
-  BiomassData<- AssignEconomicData(BiomassData) #Assign price and cost data to each stock
-  
-  BiomassData$RanCatchMSY<- F
-  
-  BiomassData$IdLevel<- 'Species'
-  
-  BiomassData$BestModel<- unique(BiomassData$BestModel[is.na(BiomassData$BestModel)==F])
-  
-  
-  BiomassData$Dbase<- 'FAO'
-  
-  OmitStatus<- AnalyzeFisheries(BiomassData,'JackStat','Year',min(BiomassData$Year):max(BiomassData$Year),RealModelSdevs,NeiModelSdevs,TransbiasBin,TransbiasIterations)
-  
-  TempJack[,c('PrmB')]<- OmitStatus$Data$BvBmsy
-  
-  CatchMSYresults<- (RunCatchMSY(OmitStatus$Data,ErrorSize,sigR,Smooth,Display,BestValues,ManualFinalYear,NumCatchMSYIterations,NumCPUs,CatchMSYTrumps))
-  
-  TempJack[,c('CmsyB','CmsyF','CmsyMSY')]<- CatchMSYresults[,c('CatchMSYBvBmsy','FvFmsy','MSY')]
-  
-  OmitStatus$Data$BvBmsySD<- NA
-  
-  CatchMSYresults<- (RunCatchMSY(OmitStatus$Data,1,sigR,Smooth,Display,BestValues,ManualFinalYear,NumCatchMSYIterations,NumCPUs,CatchMSYTrumps))
-  
-  TempJack[,c('CmsyBnoP','CmsyFnoP','CmsyMSYnoP')]<- CatchMSYresults[,c('CatchMSYBvBmsy','FvFmsy','MSY')]
-  
-  show(paste(100*(r/length(RamIds)),' % Done with JackKnife',sep=''))
-  
-  JackStore<- rbind(JackStore,TempJack)
+    TempJack<- as.data.frame(matrix(NA,nrow=dim(Omit)[1],ncol=12))
+    
+    colnames(TempJack)<- c('Assessid','Year','RamB','RamF','RamMSY','PrmB','CmsyB','CmsyF','CmsyMSY','CmsyBnoP','CmsyFnoP','CmsyMSYnoP')
+    
+    TempJack[,c('Assessid','Year','RamB','RamF','RamMSY')]<- Omit[,c('IdOrig','Year','BvBmsy','FvFmsy','MSY')]
+    
+    JackModel<- RunRegressions(Jacked,Regressions,'Real Stocks')
+    
+    RealModelFactorLevels<- NULL
+    
+    Models<- names(Regressions)
+    
+    
+    TempOmitted<- NULL
+    
+    for (m in 1:length(names(Regressions)))
+    {
+      Model<- names(Regressions)[m]
+      eval(parse(text=paste('RealModelFactorLevels$',Model,'<- RealModels$',Model,'$xlevels$SpeciesCatName',sep='')))
+    }
+    
+    Jacked<- InsertFisheryPredictions(Jacked,JackModel) #Add fishery predictions back into main dataframe
+    
+    RealModelSdevs<- CreateSdevBins(JackModel,Jacked,TransbiasBin)
+    
+    
+    AllPossible<- unique(data.frame(I(Jacked$SpeciesCatName),I(Jacked$SpeciesCat)))
+    
+    colnames(AllPossible)<- c('SpeciesCatNames','SpeciesCat')
+    
+    RamPossibleCats<- unique(RamData$SpeciesCatName)
+    
+    Models<- Models[Models!='M7']
+    
+    for (m in 1:length(Models)) #Apply models to species level fisheries
+    {
+      
+      TempModelName<- Models[m]
+      
+      eval(parse(text=paste('TempLevel<- RealModelFactorLevels$',TempModelName,sep='')))
+      
+      eval(parse(text=paste('TempModel<- RealModels$',TempModelName,sep='')))
+      
+      ProxyCats<- AssignNearestSpeciesCategory(Omit,TempLevel,AllPossible)
+      
+      Predictions<- predict(TempModel,ProxyCats$Data)
+      
+      eval(parse(text=paste('Omit$',TempModelName,'Prediction<- Predictions',sep='')))  
+    }
+    
+    BiomassData<- Omit #Only store fisheries that have some form of biomass estimates
+    
+    BiomassData$BvBmsy<- NA
+    
+    BiomassData$BestModel<- NA
+    
+    BiomassData$LogBvBmsy<- NA
+    
+    BiomassData$FvFmsy<- NA
+    
+    BiomassData$MSY<- NA
+    
+    BiomassColumns<- (grepl('BvBmsy',colnames(Omit)) | grepl('Prediction',colnames(Omit))) & grepl('LogBvBmsy',colnames(Omit))==F
+    
+    AvailableBio<- (BiomassData[,BiomassColumns])
+    
+    AvailableBioMarker<- matrix(rep((1:dim(AvailableBio)[2]),dim(AvailableBio)[1]), dim(AvailableBio)[1],dim(AvailableBio)[2],byrow=TRUE)
+    
+    AvailableBioMarker<- AvailableBioMarker*(is.na(AvailableBio)==F)
+    
+    AvailableBioMarker[AvailableBioMarker==0]<- NA
+    
+    BestModel<- apply(AvailableBioMarker,1,min,na.rm=T)
+    
+    BestBio<- NULL
+    for (b in 1:dim(AvailableBio)[1])
+    {
+      BestBio[b]<- AvailableBio[b,BestModel[b]]
+    }
+    
+    BestBio[BestModel==1]<- log(BestBio[BestModel==1])
+    
+    BestModelnames<- c('RAM',ModelNames)
+    
+    BestModelNames<- BestModelnames[sort(unique(BestModel))]
+    
+    BestModel<- as.factor((BestModel))
+    
+    levels(BestModel)<- BestModelNames
+    
+    BiomassData$BestModel<- BestModel
+    
+    BiomassData$BvBmsy<- BestBio
+    
+    BiomassData<- AssignEconomicData(BiomassData) #Assign price and cost data to each stock
+    
+    BiomassData$RanCatchMSY<- F
+    
+    BiomassData$IdLevel<- 'Species'
+    
+    BiomassData$BestModel<- unique(BiomassData$BestModel[is.na(BiomassData$BestModel)==F])
+    
+    
+    BiomassData$Dbase<- 'FAO'
+    
+    OmitStatus<- AnalyzeFisheries(BiomassData,'JackStat','Year',min(BiomassData$Year):max(BiomassData$Year),RealModelSdevs,NeiModelSdevs,TransbiasBin,TransbiasIterations)
+    
+    TempJack[,c('PrmB')]<- OmitStatus$Data$BvBmsy
+    
+    CatchMSYresults<- (RunCatchMSY(OmitStatus$Data,ErrorSize,sigR,Smooth,Display,BestValues,ManualFinalYear,NumCatchMSYIterations,NumCPUs,CatchMSYTrumps))
+    
+    TempJack[,c('CmsyB','CmsyF','CmsyMSY')]<- CatchMSYresults[,c('CatchMSYBvBmsy','FvFmsy','MSY')]
+    
+    OmitStatus$Data$BvBmsySD<- NA
+    
+    CatchMSYresults<- (RunCatchMSY(OmitStatus$Data,1,sigR,Smooth,Display,BestValues,ManualFinalYear,NumCatchMSYIterations,NumCPUs,CatchMSYTrumps))
+    
+    TempJack[,c('CmsyBnoP','CmsyFnoP','CmsyMSYnoP')]<- CatchMSYresults[,c('CatchMSYBvBmsy','FvFmsy','MSY')]
+    
+    show(paste(100*(r/length(RamIds)),' % Done with JackKnife',sep=''))
+    
+    JackStore<- rbind(JackStore,TempJack)
   } #Close if all catch loop
 }
 
