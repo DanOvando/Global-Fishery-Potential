bc326af109c6c5abbe8f8d4854bd330c0506bb89
DanOvando <dovando@bren.ucsb.edu>
2014-12-08 14:24:03 -0800

    HK

--- Functions/RunProjections.R
+++ Functions/RunProjections.R
@@ -6,17 +6,17 @@
 RunProjection<- function(Data,BaselineYear,NumCPUs)
 {
   
- # Data<-  MsyData[MsyData$CanProject==T,]
-#   Data<- MsyData[MsyData$CanProject==T & MsyData$IdOrig=='AFSC-REYEROCKBSAI-1974-2011-STACHURA',]
+  # Data<-  MsyData[MsyData$CanProject==T,]
+  #   Data<- MsyData[MsyData$CanProject==T & MsyData$IdOrig=='AFSC-REYEROCKBSAI-1974-2011-STACHURA',]
   
   #   #  
   #        BaselineYear<- 2011
   
   Data$MarginalCost<- NA
   
-#   Data$FvFmsy[Data$FvFmsy>=1.9]<- 1.9
-# 
-#   Data$BvBmsy[Data$BvBmsy>=1.9]<- 1.9
+  #   Data$FvFmsy[Data$FvFmsy>=1.9]<- 1.9
+  # 
+  #   Data$BvBmsy[Data$BvBmsy>=1.9]<- 1.9
   
   
   
@@ -51,18 +51,21 @@ RunProjection<- function(Data,BaselineYear,NumCPUs)
                              Discount=Discount,tol=tol,beta=beta,CatchSharePrice=CatchSharePrice,CatchShareCost=CatchShareCost,
                              Policies=Policies,ProjectionTime=ProjectionTime,TempStockMatrix=TempStockMatrix))
     
-#     sfInit( parallel=TRUE, cpus=NumCPUs )
-#     sfExport('Data','BaselineYear','Stocks','IdVar','bvec','Discount','tol','beta','CatchSharePrice','CatchShareCost','Policies','ProjectionTime','TempStockMatrix')
-#     #   sfExportAll()
-#     
-#     
-#     Projections <- (sfClusterApplyLB(1:(length(Stocks)), SnowProjections))
-#     sfStop()
+    #     sfInit( parallel=TRUE, cpus=NumCPUs )
+    #     sfExport('Data','BaselineYear','Stocks','IdVar','bvec','Discount','tol','beta','CatchSharePrice','CatchShareCost','Policies','ProjectionTime','TempStockMatrix')
+    #     #   sfExportAll()
+    #     
+    #     
+    #     Projections <- (sfClusterApplyLB(1:(length(Stocks)), SnowProjections))
+    #     sfStop()
   }
   if (NumCPUs==1)
-  {
+  {    
+    Projections <- (lapply(1:(length(Stocks)), SnowProjections,
+                           Data=Data,BaselineYear=BaselineYear,Stocks=Stocks,IdVar=IdVar,bvec=bvec,
+                           Discount=Discount,tol=tol,beta=beta,CatchSharePrice=CatchSharePrice,CatchShareCost=CatchShareCost,
+                           Policies=Policies,ProjectionTime=ProjectionTime,TempStockMatrix=TempStockMatrix))
     
-    Projections<- lapply(1:length(Stocks),SnowProjections)
   }
   
   TempStockMatrix<- ldply(Projections,data.frame)
@@ -75,9 +78,9 @@ RunProjection<- function(Data,BaselineYear,NumCPUs)
   Data$Policy[is.na(Data$Policy)]<- 'Historic'
   
   HistoricData<- Data$Policy=='Historic' 
-
+  
   HistoricFData<- Data$Policy=='Historic' & Data$HasRamFvFmsy==F & is.na(Data$FvFmsy) & Data$Dbase!='RAM' #FIX THIS
-
+  
   Data$FvFmsy[HistoricFData]<- (Data$Catch[HistoricFData]/Data$MSY[HistoricFData])/Data$BvBmsy[HistoricFData]
   
   c_num<- Data$Price[HistoricData]* Data$MSY[HistoricData]*  (2-Data$BvBmsyOpenAccess[HistoricData]) * Data$BvBmsyOpenAccess[HistoricData]
