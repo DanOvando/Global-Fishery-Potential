231279a97a584a55b5bcd4f28d942472d6c45ae0
DanOvando <dovando@bren.ucsb.edu>
2014-12-08 14:06:32 -0800

    Removed RAM stocks from open access pool and capped B/Bmsy values at 1.9

--- Functions/FindOpenAccess.R
+++ Functions/FindOpenAccess.R
@@ -17,20 +17,23 @@ FindOpenAccess<-function(MsyData,BaselineYear,BOAtol)
   
   for (s in 1:length(SpeciesCats))
   {
-    CategoryStocks<-Data[Data$SpeciesCatName==SpeciesCats[s] & Data$Year==BaselineYear,]
+    CategoryStocks<-Data[Data$SpeciesCatName==SpeciesCats[s] & Data$Year==BaselineYear &  Data$Dbase!='RAM',]
     
     KobeSpace<-ddply(CategoryStocks,c('IdOrig'),summarize,KobeSpot=abs(1-sum(BvBmsy+FvFmsy,na.rm=T)/2)) 
-      # Any stock on the equilibrium line has a value of 2 for the sum of BvBmsy and FvFmsy
-      # This ddply calculates the distance from the equilibrium line for each stock
-      # subset this dataset to only include stocks within a desired tolerance, make option on Master
+    # Any stock on the equilibrium line has a value of 2 for the sum of BvBmsy and FvFmsy
+    # This ddply calculates the distance from the equilibrium line for each stock
+    # subset this dataset to only include stocks within a desired tolerance, make option on Master
     
     IdsBOA<-KobeSpace$IdOrig[KobeSpace$KobeSpot<=BOAtol] # identify stocks that are within tolerance of equilibrium
-
+    
     OpenAccess$SpeciesCatName[s]<-SpeciesCats[s] # store species category
     OpenAccess$BvBmsyOpenAccess[s]<-summary(CategoryStocks$BvBmsy[CategoryStocks$IdOrig %in% IdsBOA])[2] # store BvBmsy of 25 percentile 
-    }
+  }
   
   OpenAccess<-OpenAccess[is.na(OpenAccess$BvBmsyOpenAccess)==F,]
+  
+  OpenAccess$BvBmsyOpenAccess[(OpenAccess$BvBmsyOpenAccess)>=1.9]<- 1.9
+  
   
   return(BvBmsyOpenAccess=OpenAccess)
 }
