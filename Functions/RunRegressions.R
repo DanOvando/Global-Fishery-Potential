60792631fda34f2e9d8060b1803b4b6c34d7ef0a
DanOvando <dovando@bren.ucsb.edu>
2014-12-22 15:48:55 -0600

    Fixed RunRegressions to properly ignore extended timeseries years, and place model fits back in RamData, in place of Removing InsertFisheryPredictions

--- Functions/RunRegressions.R
+++ Functions/RunRegressions.R
@@ -6,53 +6,70 @@
 RunRegressions<- function(Data,RegList,FigureName)
 {
   
-#   Data<- RamData
-#   
-#   RegList<- Regressions
-#   
-#   FigureName<- 'eh'
-   
+  #   Data<- RamData
+  #   
+  #   RegList<- Regressions
+  #   
+  #   FigureName<- 'eh'
+  
   RegNames<- names(RegList)
   
   Models<- list()
   
   for (m in 1:length(RegNames))
   {
-  
-  ModelVars<- eval(parse(text=paste('RegList$',RegNames[m],sep='')))
     
-#   WhereVars<- colnames(Data) %in% c('IdOrig',ModelVars)
-#   
-  WhereVars<- colnames(Data) %in% c(ModelVars)
-#   
+    ModelVars<- eval(parse(text=paste('RegList$',RegNames[m],sep='')))
+    
+    #   WhereVars<- colnames(Data) %in% c('IdOrig',ModelVars)
+    #   
+    WhereVars<- colnames(Data) %in% c(ModelVars)
+    #   
     fmla <- as.formula(paste("LogBvBmsy ~ ", paste( ModelVars[ModelVars!='LogBvBmsy' & ModelVars!='IdOrig'], collapse= "+")))
+        
+    RegData<- Data[Data$ExtendedTime==F,]
+    
+    UsedNames<- paste(RegData$IdOrig,RegData$Year,sep='-')
+    
+    AllNames<- paste(Data$IdOrig,Data$Year,sep='-')
+    
+    TempReg<- lm(fmla,data=RegData)
+    
+    #   TempReg<- plm(fmla , data=Data[,WhereVars],model="pooling",index=c('IdOrig','Year'),na.action='na.omit')
+       
+    Model<- ModelNames[m]  
+        
+    Omitted<- names(na.action(TempReg))
+    
+    HasPrediction<- UsedNames[(rownames(RegData) %in% Omitted)==F]
+    
+    WherePrediction<- AllNames %in% HasPrediction
+    
+    eval(parse(text=paste('Data$',Model,'Marker[WherePrediction]<- TRUE' ,sep='')))
+    
+    eval(parse(text=paste('Data$',Model,'Prediction[WherePrediction]<- TempReg$fitted.values' ,sep='')))
 
-  TempReg<- lm(fmla , data=Data[Data$ExtendedTime==F,WhereVars])
-  
-#   TempReg<- plm(fmla , data=Data[,WhereVars],model="pooling",index=c('IdOrig','Year'),na.action='na.omit')
-  
-  
-# z=predict(TempReg,Data[,WhereVars])
-# 
-# r<- TempReg$residuals
-# 
-# s<- TempReg$model$LogBvBmsy
-
-     pdf(file=paste(FigureFolder,FigureName,' ',RegNames[m],' Observed vs Predicted.pdf',sep=''))
+    # z=predict(TempReg,Data[,WhereVars])
+    # 
+    # r<- TempReg$residuals
+    # 
+    # s<- TempReg$model$LogBvBmsy
+    
+    pdf(file=paste(FigureFolder,FigureName,' ',RegNames[m],' Observed vs Predicted.pdf',sep=''))
     plot(TempReg$fitted.values+TempReg$residuals,TempReg$fitted.values,xlab='Observed Log B/Bmsy',
          ylab='Predicted Log B/Bmsy',main=paste('Model ',RegNames[m],sep=''))
     abline(0,1)
     dev.off()
-  
-#   pdf(file=paste(FigureFolder,RegNames[m],' Leverage Plots.pdf',sep=''))
-#   leveragePlots(TempReg,ask=F)
-#   dev.off()
-#   
-  show(summary(TempReg))
-  
-  eval(parse(text=paste('Models$',RegNames[m],'=TempReg',sep='')))
-  
+    
+    #   pdf(file=paste(FigureFolder,RegNames[m],' Leverage Plots.pdf',sep=''))
+    #   leveragePlots(TempReg,ask=F)
+    #   dev.off()
+    #   
+    show(summary(TempReg))
+    
+    eval(parse(text=paste('Models$',RegNames[m],'=TempReg',sep='')))
+    
   }
   
-  return(Models)
+  return(list(Models=Models,RamData=Data))
 } #Close function
