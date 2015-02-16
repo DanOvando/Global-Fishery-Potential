83deeed9ddb846ae697947e2b8b208a90481c607
DanOvando <dovando@bren.ucsb.edu>
2015-02-15 22:36:40 -0800

    First draft of functions to run monte carlo

---
+++ GFRMonteCarlo.R
@@ -0,0 +1,29 @@
+GFRMonteCarlo<- function(CatchMSYPossibleParams,PolicyStorage,ErrorVars,ErrorSize)
+{
+  
+
+  Stocks<- unique(ProjectionData$IdOrig[is.na(ProjectionData$IdOrig)==F & ProjectionData$Year==BaselineYear])
+      
+  Stocks<- Stocks[Stocks %in% CatchMSYPossibleParams$IdOrig ]
+  
+  MonteMat<- mclapply(1:length(Stocks),SnowMonteCarlo,mc.cores=NumCPUs,Stocks=Stocks,ProjectionData=ProjectionData,CatchMSYPossibleParams=CatchMSYPossibleParams,
+                        PolicyStorage=PolicyStorage,ErrorVars=ErrorVars,ErrorSize=ErrorSize,Iterations=200)
+  
+  MonteMat<- ldply(MonteMat)
+  
+  MonteMat<- MonteMat[is.infinite(MonteMat$FvFmsy)==F,]
+  
+  MonteCarlo<- ddply(MonteMat,c('Iteration','Year','Policy'),summarize,MSY=sum(MSY,na.rm=T),Profits=sum(Profits,na.rm=T),
+                     Catch=sum(Yields,na.rm=T))
+    
+  
+  quartz()
+  ggplot(data=subset(MonteCarlo,Year==2013 & Policy=='Opt'),aes(MSY))+geom_histogram(fill='steelblue2',binwidth=5)
+
+  quartz()
+  ggplot(data=subset(MonteCarlo,Year==2012 | Year==2025),aes(Catch,fill=factor(Year)))+geom_density(alpha=0.2)+facet_wrap(~Policy)
+  
+  
+
+  
+}
