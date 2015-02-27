4814f06ebc11fb6e12517043d96b107b393c059d
tclavelle <tclavelle@bren.ucsb.edu>
2015-02-26 22:41:55 -0800

    reverting to original nei

--- Functions/NearestNeighborNeis.R
+++ Functions/NearestNeighborNeis.R
@@ -115,97 +115,98 @@ NearestNeighborNeis<- function(BiomassData,MsyData,ProjData,BaselineYear)
   NeiStats$TaxonLevel[grepl("spp",NeiStats$SciName)==T]<-"Genus"  
   NeiStats$TaxonLevel[grepl("spp",NeiStats$SciName)==F]<-"Non-Genus"
   
-  nei_stock<-unique(NeiStats$SciName)
-  
-  if(NumCPUs>1)
-  {
-    tempNEIs<-(mclapply(1:length(nei_stock),SnowNEIs,nei_stock=nei_stock,NEIs=NEIs,SpeciesLevel=SpeciesLevel,NeiStats=NeiStats,allstocks=allstocks,Spec_ISSCAAP=Spec_ISSCAAP,
-                    VarsToFill=VarsToFill,NumCPUs=NumCPUs))
-  }
-
-show('Completed NEI Stats mclapply')
-
-  NEIs<-ldply(tempNEIs,data.frame)
-
-show("Completed NEI ldply")
-#   JStocks<-NA# fill this vector with the number of Jstocks for each nei group 
-#   VarBvBmsy<-NA # fill this vector with the variance in BvBmsy for each nei group 
-#   VarFvFmsy<-NA # fill this vector with the variance in FvFmsy for each nei group
-#   Year<-NA
-#   for(a in 1:nrow(NeiStats))
-#   {
-#     
-#     rm(compstocks)
-#     
-#     
-#     show(paste( round(100*(a/nrow(NeiStats))), '% Done with NeiStats',sep=''))
-#     
-#     if(NeiStats$TaxonLevel[a]=="Genus") # find scientific names for all genus level nei stocks
-#     {
-#       
-#       Genus<-unlist(str_split(NeiStats$SciName[a],pattern=" "))[1] # pull out genus
-#       
-#       WhereComp<-grepl(Genus,Spec_ISSCAAP$Species_AFSIS) # search for species names in that genus
-#       
-#       compstocks<-unique(Spec_ISSCAAP$Species_AFSIS[WhereComp]) # pull out species names
-#       
-#     } 
-#     
-#     if(NeiStats$TaxonLevel[a]=="Non-Genus" & (NeiStats$SciName[a] %in% Spec_ISSCAAP$Family)) # determine if non-genus stock is a family name
-#     {  
-#       family<-NeiStats$SciName[a] # if so find species within that family
-#       
-#       WhereComp<-Spec_ISSCAAP$Family==family
-#       
-#       compstocks<-unique(Spec_ISSCAAP$Species_AFSIS[WhereComp])
-#     }
-#     
-#     if(NeiStats$TaxonLevel[a]=="Non-Genus" & (tolower(NeiStats$SciName[a]) %in% tolower(Spec_ISSCAAP$Order))) # determine if non-genus stock is an order name
-#     {  
-#       
-#       order<-toupper(NeiStats$SciName[a]) # order of nei stock (translate to uppercase to match sheet)
-#       
-#       WhereComp<-Spec_ISSCAAP$Order==order
-#       
-#       compstocks<-unique(Spec_ISSCAAP$Species_AFSIS[WhereComp])
-#     }
-#     
-#     if(exists('compstocks'))
-#     {
-#       
-#       ComparisonStocks<-SpeciesLevel[SpeciesLevel$SciName %in% compstocks,] # pulling comparable stocks from the globe
-#       #       ComparisonStocks<-SpeciesLevel[SpeciesLevel$SciName %in% compstocks  &
-#       #                                        grepl((NeiStats$RegionFAO[a]),SpeciesLevel$RegionFAO ) & is.na(SpeciesLevel$RegionFAO)==F,]
-#       #       
-#       if(nrow(ComparisonStocks)>0)
-#       {
-#         
-#         for (p in 1:length(LongPols))
-#         {
-#           results<-ddply(ComparisonStocks[ComparisonStocks$Policy==LongPols[p],],c("Year"),summarize,BvBmsy25=quantile(BvBmsy,c(0.25),na.rm=T),FvFmsy75=quantile(FvFmsy,c(0.75),na.rm=T),
-#                          MedianR=median(r,na.rm=T),MedianK=median(k,na.rm=T),MedianPrice=median(Price,na.rm=T),MedianCost=median(MarginalCost,na.rm=T),JStocks=length(unique(IdOrig)),VarBvBmsy=var(BvBmsy,na.rm=T),VarFvFmsy=var(FvFmsy,na.rm=T))
-#           
-#           Year<-append(Year,results$Year,after=length(Year))
-#           JStocks<-append(JStocks,results$JStocks,after=length(JStocks))
-#           VarBvBmsy<-append(VarBvBmsy,results$VarBvBmsy,after=length(VarBvBmsy))
-#           VarFvFmsy<-append(VarFvFmsy,results$VarFvFmsy,after=length(VarFvFmsy))
-#           
-#           for (b in 1:nrow(results))
-#           {
-#             #             WhereNei<- NEIs$SciName==NeiStats$SciName[a] & grepl((NeiStats$RegionFAO[a]),NEIs$RegionFAO ) & NEIs$Year==results$Year[b]  & is.na(NEIs$RegionFAO)==F & NEIs$Policy==LongPols[p]
-#             WhereNei<- NEIs$SciName==NeiStats$SciName[a] & NEIs$Year==results$Year[b] & NEIs$Policy==LongPols[p]            
-#             NEIs[WhereNei,VarsToFill]<-results[b,c("BvBmsy25", "FvFmsy75", "MedianR", "MedianK","MedianPrice", "MedianCost")]
-#             NEIs$CanProject[WhereNei]<- TRUE
-#             
-#           } 
-#         } # Close Policy loop
-#         #         JStocks[a]<- (unique(results$JStocks))
-#       } # Close ComparisonStocks if
-#     } # Close compstocks if
-#   } # Close NeiStats loop
-#   # Make data frame from JStocks, VarBvBmsy, and FvFmsy
+#   nei_stock<-unique(NeiStats$SciName)
 #   
-#   NeiDiagnostics<-data.frame(cbind(Year,JStocks,VarBvBmsy,VarFvFmsy))
+#   if(NumCPUs>1)
+#   {
+#     tempNEIs<-(mclapply(1:length(nei_stock),SnowNEIs,nei_stock=nei_stock,NEIs=NEIs,SpeciesLevel=SpeciesLevel,NeiStats=NeiStats,allstocks=allstocks,Spec_ISSCAAP=Spec_ISSCAAP,
+#                     VarsToFill=VarsToFill,NumCPUs=NumCPUs))
+#   }
+# 
+# show('Completed NEI Stats mclapply')
+# 
+#   NEIs<-ldply(tempNEIs,data.frame)
+# 
+# show("Completed NEI ldply")
+
+  JStocks<-NA# fill this vector with the number of Jstocks for each nei group 
+  VarBvBmsy<-NA # fill this vector with the variance in BvBmsy for each nei group 
+  VarFvFmsy<-NA # fill this vector with the variance in FvFmsy for each nei group
+  Year<-NA
+  for(a in 1:nrow(NeiStats))
+  {
+    
+    rm(compstocks)
+    
+    
+    show(paste( round(100*(a/nrow(NeiStats))), '% Done with NeiStats',sep=''))
+    
+    if(NeiStats$TaxonLevel[a]=="Genus") # find scientific names for all genus level nei stocks
+    {
+      
+      Genus<-unlist(str_split(NeiStats$SciName[a],pattern=" "))[1] # pull out genus
+      
+      WhereComp<-grepl(Genus,Spec_ISSCAAP$Species_AFSIS) # search for species names in that genus
+      
+      compstocks<-unique(Spec_ISSCAAP$Species_AFSIS[WhereComp]) # pull out species names
+      
+    } 
+    
+    if(NeiStats$TaxonLevel[a]=="Non-Genus" & (NeiStats$SciName[a] %in% Spec_ISSCAAP$Family)) # determine if non-genus stock is a family name
+    {  
+      family<-NeiStats$SciName[a] # if so find species within that family
+      
+      WhereComp<-Spec_ISSCAAP$Family==family
+      
+      compstocks<-unique(Spec_ISSCAAP$Species_AFSIS[WhereComp])
+    }
+    
+    if(NeiStats$TaxonLevel[a]=="Non-Genus" & (tolower(NeiStats$SciName[a]) %in% tolower(Spec_ISSCAAP$Order))) # determine if non-genus stock is an order name
+    {  
+      
+      order<-toupper(NeiStats$SciName[a]) # order of nei stock (translate to uppercase to match sheet)
+      
+      WhereComp<-Spec_ISSCAAP$Order==order
+      
+      compstocks<-unique(Spec_ISSCAAP$Species_AFSIS[WhereComp])
+    }
+    
+    if(exists('compstocks'))
+    {
+      
+      ComparisonStocks<-SpeciesLevel[SpeciesLevel$SciName %in% compstocks,] # pulling comparable stocks from the globe
+      #       ComparisonStocks<-SpeciesLevel[SpeciesLevel$SciName %in% compstocks  &
+      #                                        grepl((NeiStats$RegionFAO[a]),SpeciesLevel$RegionFAO ) & is.na(SpeciesLevel$RegionFAO)==F,]
+      #       
+      if(nrow(ComparisonStocks)>0)
+      {
+        
+        for (p in 1:length(LongPols))
+        {
+          results<-ddply(ComparisonStocks[ComparisonStocks$Policy==LongPols[p],],c("Year"),summarize,BvBmsy25=quantile(BvBmsy,c(0.25),na.rm=T),FvFmsy75=quantile(FvFmsy,c(0.75),na.rm=T),
+                         MedianR=median(r,na.rm=T),MedianK=median(k,na.rm=T),MedianPrice=median(Price,na.rm=T),MedianCost=median(MarginalCost,na.rm=T),JStocks=length(unique(IdOrig)),VarBvBmsy=var(BvBmsy,na.rm=T),VarFvFmsy=var(FvFmsy,na.rm=T))
+          
+          Year<-append(Year,results$Year,after=length(Year))
+          JStocks<-append(JStocks,results$JStocks,after=length(JStocks))
+          VarBvBmsy<-append(VarBvBmsy,results$VarBvBmsy,after=length(VarBvBmsy))
+          VarFvFmsy<-append(VarFvFmsy,results$VarFvFmsy,after=length(VarFvFmsy))
+          
+          for (b in 1:nrow(results))
+          {
+            #             WhereNei<- NEIs$SciName==NeiStats$SciName[a] & grepl((NeiStats$RegionFAO[a]),NEIs$RegionFAO ) & NEIs$Year==results$Year[b]  & is.na(NEIs$RegionFAO)==F & NEIs$Policy==LongPols[p]
+            WhereNei<- NEIs$SciName==NeiStats$SciName[a] & NEIs$Year==results$Year[b] & NEIs$Policy==LongPols[p]            
+            NEIs[WhereNei,VarsToFill]<-results[b,c("BvBmsy25", "FvFmsy75", "MedianR", "MedianK","MedianPrice", "MedianCost")]
+            NEIs$CanProject[WhereNei]<- TRUE
+            
+          } 
+        } # Close Policy loop
+        #         JStocks[a]<- (unique(results$JStocks))
+      } # Close ComparisonStocks if
+    } # Close compstocks if
+  } # Close NeiStats loop
+  # Make data frame from JStocks, VarBvBmsy, and FvFmsy
+  
+  NeiDiagnostics<-data.frame(cbind(Year,JStocks,VarBvBmsy,VarFvFmsy))
   
   NEIs<- NEIs[NEIs$CanProject==T,]
   
@@ -244,7 +245,7 @@ show("Completed NEI ldply")
       
 #       show(s)
     } # close stock loop
- show(p)
+#  show(p)
 } # close policy loop
   
   Biomass<- NEIs[NEIs$Policy=='Historic',colnames(NEIs) %in% colnames(BiomassData)]
