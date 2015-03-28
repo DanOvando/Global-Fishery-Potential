3d8656ae7c8ef1d7e23ff113c41534dbe4190034
DanOvando <dovando@bren.ucsb.edu>
2015-03-27 19:37:40 -0700

    Added in profits and NPV for Chris

--- Functions/ProjectionValidation.R
+++ Functions/ProjectionValidation.R
@@ -10,21 +10,25 @@ ProjectionValidation<-function(ProjectionData,BaselineYear)
 {
 #   input<-ProjectionData[ProjectionData$Year==BaselineYear & ProjectionData$IdLevel=='Species',c('IdOrig','Country','CatchShare','Year','BvBmsy','FvFmsy','MSY','','Price','MarginalCost')]
 
-  input<-ProjectionData[ProjectionData$Year==BaselineYear & ProjectionData$IdLevel=='Species',c('IdOrig','Country','CatchShare','Year','BvBmsy','FvFmsy','MSY','g','phi','Price','MarginalCost')]
+#   input<-ProjectionData[ProjectionData$Year==BaselineYear & ProjectionData$IdLevel=='Species',c('IdOrig','Country','CatchShare','Year','BvBmsy','FvFmsy','MSY','g','phi','Price','MarginalCost')]
+  
+  input<-ProjectionData[ProjectionData$Year==BaselineYear,c('IdOrig','Country','CatchShare','Year','BvBmsy','FvFmsy','MSY','g','phi','Price','MarginalCost','Profits')]
+  
   
   output<-ProjectionData[ProjectionData$Year==max(ProjectionData$Year,na.rm=T) & ProjectionData$IdOrig %in% input$IdOrig,
                          c('IdOrig','Country','CatchShare','Year','Policy','BvBmsy','FvFmsy','MSY','g','phi','MarginalCost','Profits','Biomass','Catch')]
 
   npv<-ddply(ProjectionData[ProjectionData$IdOrig %in% output$IdOrig & ProjectionData$Policy!='Historic',],c('IdOrig','Policy'),summarize,FinalNPV=sum(DiscProfits,na.rm=T))
   
-  for(a in 1:nrow(npv))
-  {
-    where<-output$IdOrig==npv$IdOrig[a] & output$Policy==npv$Policy[a]
-    
-    output$NPV[where]<-npv$FinalNPV[a]
-  }
-
-  output2<-output[,c('IdOrig','Country','CatchShare','Policy','Year','Catch','Biomass','BvBmsy','NPV','FvFmsy')]
+  npv$name<- paste(npv$IdOrig,npv$Policy,sep='-')
+  
+  output$name<- paste(output$IdOrig,output$Policy,sep='-')
+  
+  output<-join(output,npv,by='name')
+  
+  output<- output[order(output$IdOrig),]
+  
+  output2<-output[,c('IdOrig','Country','CatchShare','Policy','Year','Catch','Biomass','BvBmsy','FinalNPV','FvFmsy')]
   
   write.csv(input,file=paste(ResultFolder,'Parameters to Test DynOpt and Projections.csv',sep=''))
   
