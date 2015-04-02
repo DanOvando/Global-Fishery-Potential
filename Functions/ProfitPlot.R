88d33918174880d6f5d985963c5221086721b3a0
DanOvando <dovando@bren.ucsb.edu>
2015-04-02 08:15:24 -0700

    scratch

--- Functions/ProfitPlot.R
+++ Functions/ProfitPlot.R
@@ -1,11 +1,11 @@
-AB=a[is.na(a$Profits)==F,]
-
-AB$WTF<- AB$Price*AB$BvBmsy*AB$FvFmsy*AB$MSY-AB$MarginalCost*(AB$FvFmsy*AB$g)^1.3
-
-
-quartz()
-AB$CatchShareRam<- AB$CatchShare==1 & AB$Dbase=='RAM'
-
-ggplot(AB,aes(x=Profits,y=WTF,color=CatchShareRam))+geom_point(alpha=0.5)
-
-ggplot(AB,aes(x=Profits,fill=Policy))+geom_histogram(alpha=0.5)
+# AB=a[is.na(a$Profits)==F,]
+# 
+# AB$WTF<- AB$Price*AB$BvBmsy*AB$FvFmsy*AB$MSY-AB$MarginalCost*(AB$FvFmsy*AB$g)^1.3
+# 
+# 
+# quartz()
+# AB$CatchShareRam<- AB$CatchShare==1 & AB$Dbase=='RAM'
+# 
+# ggplot(AB,aes(x=Profits,y=WTF,color=CatchShareRam))+geom_point(alpha=0.5)
+# 
+# ggplot(AB,aes(x=Profits,fill=Policy))+geom_histogram(alpha=0.5)
