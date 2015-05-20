d7798ee564c47a3381d218a797125baefdddb5f0
tclavelle <tclavelle@bren.ucsb.edu>
2015-05-20 10:34:30 -0700

    adjusted validation spreadsheets and gitignore

--- Functions/ProjectionValidation.R
+++ Functions/ProjectionValidation.R
@@ -6,6 +6,8 @@
 ##
 ###########################################
 
+# Replace FinalNPV with Profits_2050
+
 ProjectionValidation<-function(ProjectionData,BaselineYear)
 {
 #   input<-ProjectionData[ProjectionData$Year==BaselineYear & ProjectionData$IdLevel=='Species',c('IdOrig','Country','CatchShare','Year','BvBmsy','FvFmsy','MSY','','Price','MarginalCost')]
@@ -16,7 +18,7 @@ ProjectionValidation<-function(ProjectionData,BaselineYear)
   
   
   output<-ProjectionData[ProjectionData$Year==max(ProjectionData$Year,na.rm=T) & ProjectionData$IdOrig %in% input$IdOrig,
-                         c('IdOrig','Country','CatchShare','Year','Policy','BvBmsy','FvFmsy','MSY','g','phi','MarginalCost','Profits','Biomass','Catch')]
+                         c('IdOrig','Country','CatchShare','Year','Policy','BvBmsy','FvFmsy','MSY','g','phi','MarginalCost','Profits','Biomass','Catch','Price')]
 
   npv<-ddply(ProjectionData[ProjectionData$IdOrig %in% output$IdOrig & ProjectionData$Policy!='Historic',],c('IdOrig','Policy'),summarize,FinalNPV=sum(DiscProfits,na.rm=T))
   
@@ -28,7 +30,7 @@ ProjectionValidation<-function(ProjectionData,BaselineYear)
   
   output<- output[order(output$IdOrig),]
   
-  output2<-output[,c('IdOrig','Country','CatchShare','Policy','Year','Catch','Biomass','BvBmsy','FinalNPV','FvFmsy')]
+  output2<-output[,c('IdOrig','Country','CatchShare','Policy','Year','Catch','Biomass','BvBmsy','Profits','FvFmsy','Price ')]
   
   write.csv(input,file=paste(ResultFolder,'Parameters to Test DynOpt and Projections.csv',sep=''))
   
