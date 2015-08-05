978be396fd92578f97c0be46e714d77ff35a5ddb
DanOvando <dovando@bren.ucsb.edu>
2015-08-05 14:58:41 -0700

    build elastic potential into build BAU

--- Functions/BuildPolicyBAUs.R
+++ Functions/BuildPolicyBAUs.R
@@ -12,7 +12,7 @@
 
 
 
-BuildPolicyBAUs<-function(ProjectionData,BaselineYear)
+BuildPolicyBAUs<-function(ProjectionData,BaselineYear, elastic_demand = F)
 {
   
   ### 1) "Business As Usual Pessimistic" Where all non RAM and Catch share stocks go to Open Access
@@ -20,6 +20,8 @@ BuildPolicyBAUs<-function(ProjectionData,BaselineYear)
   # RAM - F current forever
   # Catch shares - Opt
   # All others - Open Access
+  # 
+  
   ram<-ProjectionData[ProjectionData$Policy=='StatusQuoFForever' & ProjectionData$Dbase=='RAM' & ProjectionData$CatchShare!=1,]
   
   ramids<-unique(ram$IdOrig)
@@ -33,9 +35,29 @@ BuildPolicyBAUs<-function(ProjectionData,BaselineYear)
   other<-ProjectionData[(ProjectionData$IdOrig %in% c(otherids)) & ProjectionData$Policy=='StatusQuoOpenAccess',]
   
   BAUpess<-rbind(ram,cs,other)
-
+  
   BAUpess$Policy<-'Business As Usual Pessimistic'
-    
+  
+  elastic_BAUpess <- elastic_projection(poldata = BAUpess,oa_ids = otherids, elasticity = -1,
+                                        discount = Discount, base_year = BaselineYear )  
+  
+  #   nonelastic<- subset(BAUpess, (IdOrig %in% otherids))
+  # 
+  #   elastic<- subset(Test, (IdOrig %in% otherids))
+  #   
+  #   browser()
+  #   
+  #   quartz()
+  #   ggplot(elastic,aes(factor(Year),log(Price))) + geom_boxplot()
+  #   
+  #   elastic_summary <- elastic %>%
+  #     group_by(Year) %>%
+  #     summarize(TotalCatch = sum(Catch,na.rm = T), MeanPrice = mean(Price, na.rm = T))
+  #   
+  #   quartz()
+  #   ggplot(elastic_summary,aes(TotalCatch,(MeanPrice))) + geom_point()
+  #   
+  
   ### 2) "Business As Usual Current Management"
   
   # RAM - Fmsy
@@ -58,6 +80,8 @@ BuildPolicyBAUs<-function(ProjectionData,BaselineYear)
   
   BAUoptim$Policy<-'Business As Usual'
   
+  elastic_BAUoptim <- elastic_projection(poldata = BAUoptim,oa_ids = overFFids, elasticity = -1, discount = Discount, base_year = BaselineYear)  
+  
   ### 3 & 4) "Catch Share Three" and "Fmsy Three" - Adjust results for CS and Fmsy policies so that the policy is not applied to underfished/underfishing stocks
   
   PolicyOverFFids<-ProjectionData$IdOrig[ProjectionData$Year==BaselineYear & (ProjectionData$BvBmsy<1 | ProjectionData$FvFmsy>1)] # overfished/overfishing stocks
@@ -73,12 +97,16 @@ BuildPolicyBAUs<-function(ProjectionData,BaselineYear)
   McRam<- subset(ProjectionData, (IdOrig %in% PolicyUnderFFids) & Policy=='StatusQuoFForever' & Dbase=='RAM' & CatchShare!=1)
   
   McCatchShares<-subset(ProjectionData, (IdOrig %in% PolicyUnderFFids) & Policy=='Opt' & CatchShare==1)
-    
+  
   Myctophids<- subset(ProjectionData, (IdOrig %in% PolicyUnderFFids) & Policy=='StatusQuoBForever' & CatchShare!=1 & Dbase!='RAM')
   
   CatchShareThree<-rbind(CsThree,McRam,McCatchShares,Myctophids) # combine Catch Share results for overfished/overfishing results with B current forever results for mctofids
   
   CatchShareThree$Policy<-'Catch Share Three'
+  
+  elastic_CatchShareThree <- elastic_projection(poldata = CatchShareThree, oa_ids = 'none', elasticity = -1, discount = Discount, base_year = BaselineYear)  
+  
+  
   
   # 4) Fmsy 
   
@@ -88,9 +116,17 @@ BuildPolicyBAUs<-function(ProjectionData,BaselineYear)
   
   FmsyThree$Policy<-'Fmsy Three'
   
-  ### Bind all composite policies to ProjectionData
-  
+  elastic_FmsyThree <- elastic_projection(poldata = FmsyThree, oa_ids = 'none', elasticity = -1, discount = Discount, base_year = BaselineYear)  
+
+    ### Bind all composite policies to ProjectionData
+  if (elastic_demand == F){
+    
   ProjectionData<-rbind(ProjectionData,BAUpess,BAUoptim,CatchShareThree,FmsyThree)
+  }
+  if (elastic_demand == T){
+    ProjectionData<-rbind(ProjectionData,elastic_BAUpess,elastic_BAUoptim,elastic_CatchShareThree,elastic_FmsyThree)
+    
+  }
   
   return(ProjectionData)
 }
