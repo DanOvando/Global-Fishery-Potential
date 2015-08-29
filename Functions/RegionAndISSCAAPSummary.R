5a01f694ca3c6eb1f338e151cc7245067093ab24
tclavelle <tclavelle@bren.ucsb.edu>
2015-08-28 17:36:45 -0700

    new figure one kobe plots

--- Functions/RegionAndISSCAAPSummary.R
+++ Functions/RegionAndISSCAAPSummary.R
@@ -135,7 +135,75 @@ RegionFaoAndISSCAAPSummary<-function(ProjectionData,BaselineYear)
   }
   
   dev.off()
+
+  # Make Figure on Kobe plot with regions 67,27,61,71
   
+  zone<-c('Northeast Pacific','Northeast Atlantic','Northwest Pacific','Western Central Pacific')
+  
+  codes<-c('67','27','61','71') # codes
+  
+  names<-data.frame(zone,codes,stringsAsFactors=F) # df of names and corresponding codes for every FAO major region
+  
+  #==the ones that work...wtf
+  justX	<-4
+  justY	<-1
+  noAx	<-2
+  bothAx	<-3
+  
+  pdf(file=paste(FigureFolder,'Figure One Kobe Plots.pdf',sep=''))
+  
+  par(mfrow=c(2,2),mar=c(.1,.1,.1,.1),oma=c(4,4,1,1))
+  
+  for(a in 1:length(codes))
+  {
+    temp<-data[data$Year==BaselineYear & grepl(codes[a],data$RegionFAO),]
+    
+    tempName<-names$zone[names$code==codes[a]]
+    
+    if(nrow(temp)>0)
+    {
+      
+      if (any(temp$BvBmsy<0,na.rm=T)){temp$BvBmsy<- exp(temp$BvBmsy)}
+      
+      BaselineData<- subset(temp,subset=Year==BaselineYear & is.na(FvFmsy)==F & is.na(BvBmsy)==F)
+      
+      BaselineData$FvFmsy[BaselineData$FvFmsy>4]<- 4
+      
+      BaselineData$BvBmsy[BaselineData$BvBmsy>2.5]<- 2.5
+      
+      DensityData<- (kde2d(BaselineData$BvBmsy,BaselineData$FvFmsy,n=100,lims=c(0,2.5,0,4)))
+      
+      FTrend<- ddply(temp,c('Year','Dbase'),summarize,FTrend=mean(FvFmsy,na.rm=T))
+      
+      KobeColors<- colorRampPalette(c("lightskyblue", "white",'gold1'))
+      
+      DbaseColors<- colorRampPalette(c('Red','Black','Green'))
+      
+      image(DensityData$x,DensityData$y,DensityData$z,xlim=c(0,2.5),ylim=c(0,4),col=KobeColors(50),xaxt='n',yaxt='n',bty='o',las=1)
+      if(!is.na(match(a,justY)))
+        axis(side=2,at=seq(1,3),las=1)
+      if(!is.na(match(a,justX)))
+        axis(side=1,at=seq(0,2,.5),las=1)
+      if(!is.na(match(a,bothAx)))
+      {
+        axis(side=2,at=seq(1,3),las=1)      
+        axis(side=1,at=seq(0,2,.5),las=1)
+        mtext(outer=T,side=1,expression(B/B[MSY]),line=2)
+        mtext(outer=T,side=2,expression(F/F[MSY]),line=2)
+      }
+      
+      points(BaselineData$BvBmsy,BaselineData$FvFmsy,pch=16,col=(as.factor(BaselineData$Dbase)),cex=0.75+(BaselineData$Catch)/max(BaselineData$Catch,na.rm=T))
+      box()
+      abline(h=1,v=1,lty=2)
+      legend("topright",tempName,bty='n')
+      # legend('center',legend=unique(BaselineData$Dbase),pch=16,col=(as.factor(unique(BaselineData$Dbase))),cex=0.7,bty='n') 
+    } 
+    #     show(a)
+  }
+  
+  dev.off()
+  
+    
   return(list(StatusByISSCAAP=current,StatusByRegion=RegionStatus))
 }
 
