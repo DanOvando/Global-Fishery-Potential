fa53ec6d95bcee41b7b67bdaff44a74121505691
DanOvando <dovando@bren.ucsb.edu>
2015-10-26 17:43:32 -0700

    just some house keeping

--- fuckit.R
+++
@@ -1,3 +0,0 @@
-sapply(list.files(pattern="[.]R$", path="Functions", full.names=TRUE), source)
-
-fuuuuck <- run_gfr_diagnostics(runfolder = '5.2 global demand common phi', do_expanded_montecarlo = T, mciterations = 2)
